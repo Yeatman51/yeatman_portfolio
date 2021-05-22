609f5eaae1f126ba170351e296fae3ac5b63962a
Jon Yeatman <jonlyeatman@gmail.com>
2021-05-22 14:18:31 -0400

    Lazy loading on designer pg

--- public/js/script.js
+++ public/js/script.js
@@ -14,14 +14,45 @@
 //     });
 // });
 
-const images = document.getElementsByClassName("gd-gallery-img");
+// const images = document.getElementsByClassName("gd-gallery-img");
 
-const imgOptions = {};
+// const imgOptions = {};
+// const imgObserver = new IntersectionObserver((entries, imgObserver) => {
+//   entries.forEach((entry) => {
+//     if (!entry.isIntersecting) return;
+
+//     const img = entry.target;
+//     imgObserver.unobserve(entry.target);
+//   });
+// }, imgOptions);
+
+const images = document.querySelectorAll("[data-src]");
+
+function preloadImage(img) {
+  const src = img.getAttribute("data-src");
+  if (!src) {
+    return;
+  }
+
+  img.src = src;
+}
+
+const imgOptions = {
+  threshold: 0,
+  rootMargin: "0px 0px 200px 0px"
+};
+
 const imgObserver = new IntersectionObserver((entries, imgObserver) => {
-  entries.forEach((entry) => {
-    if (!entry.isIntersecting) return;
-
-    const img = entry.target;
-    imgObserver.unobserve(entry.target);
-  });
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      preloadImage(entry.target);
+      imgObserver.unobserve(entry.target);
+    }
+  })
 }, imgOptions);
+
+images.forEach(image =>  {
+  imgObserver.observe(image);
+});
